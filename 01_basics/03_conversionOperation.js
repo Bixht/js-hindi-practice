3f0df00a5f230d3aca142e7cca97e57d43a230b7
Bixht <127079528+Bixht@users.noreply.github.com>
2025-03-16 12:09:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "88"
+
+// console.log(typeof score);
+// console.log((typeof score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "88" = 88
+// "88abc" = NaN "Not a Number"
+// true = 1; false = 0;
+
+// let isLoggedIn = 88
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false;
+// "" = false
+// "bisht" = true
+
+let thisNumber = 88
+
+let upperNumber = String(thisNumber)
+console.log(upperNumber);
+console.log(typeof upperNumber);
