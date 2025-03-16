0a842ca221cef59b597e04fb67753a57620f3256
Bixht <127079528+Bixht@users.noreply.github.com>
2025-03-16 07:44:38 +0000

    changed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 885142
+let accountEmail = "neeraj884488@gmail.com"
+var accountPassword ="885142"
+accountCity = "Uttrakhand"
+let accountState;
+
+//accountId = 2 // not to use 
+
+accountEmail = "html5neer@gmail.com"
+accountPassword = "884488"
+accountCity = "Dehradun"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefere not to use var
+because of issue in block scope and functonal scope
+*/
