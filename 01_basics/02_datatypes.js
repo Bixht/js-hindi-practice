ed88e723bd364f5f81ad58988bfe33ce98431f6e
Bixht <127079528+Bixht@users.noreply.github.com>
2025-03-16 08:45:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert( 3 + 3 ) // we are using nodejs not, not browser
+
+console.log(3 + 3);
+// Code readability should be high
+console.log("Bisht");
+
+let name ="Neeraj"
+let age = 22
+let isLoggedIn = true
+let state = null
+
+
+/* Primitive Data Types */
+
+
+// Null = Standalone value 
+// Number = 4
+// Boolean = True / False
+// Bigint
+// String = ""
+// Symbol = unique
+// Undefined
+
+// object
+
+console.log(typeof isLoggedIn);
