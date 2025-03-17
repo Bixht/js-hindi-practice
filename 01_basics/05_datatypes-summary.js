119cc072cfcfb0b57ca0a6daa3727cf1bcf8e0bf
Bixht <127079528+Bixht@users.noreply.github.com>
2025-03-17 20:04:02 +0000

    datatype-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 Types Of Primitive //
+
+// N,N,B,B,S,S,U //This is the short form of Primitives
+// Null, Number, Boolean, BigInt, String, Symbol, Undefined //
+
+const score = 800           //-------------------
+                                              //|---Number                
+const scoreValue = 800.8    //-------------------
+
+const loggedIn = true       //-------------------Boolean
+const indoreTemp = null     //-------------------Null
+let myEmail;                //-------------------Undefined
+
+const id = Symbol('888')    //------------------
+                                         //    | Symbol
+const newId = Symbol('888') //------------------
+
+console.log(id === newId);
+
+const bigNumm = 88888888888n  //----------------BigInt
+
+// (Non Primitive Datatypes)
+
+//Array, Objects, Functions
+
+const superBootie = ["dalchini", "tejpatta", "kadhipatta"];
+let bioData = {
+    name : "bisht",
+    age : 22,
+}
+console.log(typeof age);
+
+
+const kachraSeth = function(){
+    console.log("Hello Raju");
+}
+
+console.log(typeof kachraSeth);
+
