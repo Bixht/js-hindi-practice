44aa8b7b263753c8a6573c477295ac70855a5cd5
Bixht <127079528+Bixht@users.noreply.github.com>
2025-03-19 19:42:16 +0000

    nums & maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,40 @@
+const score = 888
+// console.log(score);
+
+const balance = new Number(666)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.456
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+ //++++++++++++++   Maths   +++++++++++//
+
+//  console.log(Math);
+//  console.log(Math.abs(-4));
+//  console.log(Math.round(8.6));
+//  console.log(Math.ceil(4.6));
+//  console.log(Math.floor(9.6));
+//  console.log(Math.min(9,5,7,3));
+//  console.log(Math.max(9,5,7,3));
+ 
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 11
+const max = 22
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
